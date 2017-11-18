71fc06f50dc6cc6739c76a070f65d2d8ed294920
DESKTOP-96GJUGK\antho <anthony.nicolosi78@gmail.com>
2017-11-18 12:17:26 -0700

    Overrwrote OnOverlapBegin and OnOverlapEnd in PlatformTrigger. Registered callbacks in PlatformTrigger.BeginPlay, did not work in constructor. I think adding the callbacks in BeginPlayer reads a little better since it involves interactions with the game engine systems. Constrcutors should be reserved for member intiailization, etc. Lecture 14

--- Source/PuzzlePlatforms/PlatformTrigger.cpp
+++ Source/PuzzlePlatforms/PlatformTrigger.cpp
@@ -5,22 +5,33 @@
 APlatformTrigger::APlatformTrigger()
 {
 	PrimaryActorTick.bCanEverTick = true;
-		
+
 	TriggerVolume = CreateDefaultSubobject<UBoxComponent>(FName("TriggerVolume"));
 	if (!ensure(TriggerVolume != nullptr)) return;
-	
+
 	RootComponent = TriggerVolume;
 }
 
 void APlatformTrigger::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	TriggerVolume->OnComponentBeginOverlap.AddDynamic(this, &APlatformTrigger::OnOverlapBegin);
+	TriggerVolume->OnComponentEndOverlap.AddDynamic(this, &APlatformTrigger::OnOverlapEnd);
 }
 
 void APlatformTrigger::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void APlatformTrigger::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Activated"));
+}
+
+void APlatformTrigger::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Deactivated"));
 }
 
--- Source/PuzzlePlatforms/PlatformTrigger.h
+++ Source/PuzzlePlatforms/PlatformTrigger.h
@@ -10,14 +10,19 @@ UCLASS()
 class PUZZLEPLATFORMS_API APlatformTrigger : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+
+public:
 	// Sets default values for this actor's properties
 	APlatformTrigger();
 
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -25,5 +30,4 @@ protected:
 private:
 	UPROPERTY(VisibleAnywhere)
 	class UBoxComponent* TriggerVolume;
-	
 };
